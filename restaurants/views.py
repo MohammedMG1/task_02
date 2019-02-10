08be7b02bfb84486e00e32a5fe8abdd92a7cfccd
Mohammed Alotaibi <imohammed.mg@gmail.com>
2019-02-10 13:58:00 +0300

    Fatima&Mohammed Project

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# Create your views here.
+def my_first_view(request):
+    context = {
+        'msg': "Hello World!",
+        
+    }
+    return render(request, 'test.html', context)
